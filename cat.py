022e5415605454b17ebbb5f2d5b80339a28ddcaa
danielli <daniel.mail.com>
2018-11-24 19:35:08 +0800

    PEP8 by master

--- cat.py
+++ cat.py
@@ -1,9 +1,10 @@
 import os
 import urllib.request
+# edit by master
 
 res = urllib.request.urlopen('http://placekitten.com/800/800')
 cat_img = res.read()
 
 with open('cat_800_800.jpg', 'wb') as f:
     f.write(cat_img)
-print(os.getcwd())
+print(os.getcwd())  
